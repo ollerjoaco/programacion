ff13fe676446913a12812bd5e8622bf8b86d8d48
ollerjoaco <57648434+ollerjoaco@users.noreply.github.com>
2020-03-13 09:26:22 -0300

    Add files via upload

---
+++ tateti_oller.py
@@ -0,0 +1,431 @@
+print("TATETI")
+matriz=[[0,0,0],[0,0,0],[0,0,0]]
+
+#7 SON X Y 8 SON O
+print(matriz[0])
+print(matriz[1])
+print(matriz[2])
+print("Bienvenido al TATETI del Abuelo")
+print("Recuerde que las X son representadas con 7 y las O con 8")
+print("Primero juegan los 7!")
+print("Elija la fila de la 0 a la 2, siendo la primer fila el valor 0")
+primer_mov_fila=int (input())
+
+if primer_mov_fila==0:
+	print("Elija la posicion de la 0 a la 2, siendo la primera posicion el valor 0")
+	primer_mov_pos=int (input())
+	
+	if primer_mov_pos==0:
+		matriz[0][0]=7
+	elif primer_mov_pos==1:
+		matriz[0][1]=7
+	else:
+		matriz[0][2]=7
+	
+	print(matriz[0])
+	print(matriz[1])
+	print(matriz[2])
+elif primer_mov_fila==1:
+	print("Elija la posicion de la 0 a la 2, siendo la primera posicion el valor 0")
+	primer_mov_pos=int (input())
+	
+	if primer_mov_pos==0:
+		matriz[1][0]=7
+	elif primer_mov_pos==1:
+		matriz[1][1]=7
+	else:
+		matriz[1][2]=7
+	
+	print(matriz[0])
+	print(matriz[1])
+	print(matriz[2])
+else:
+	print("Elija la posicion de la 0 a la 2, siendo la primera posicion el valor 0")
+	primer_mov_pos=int (input())
+	
+	if primer_mov_pos==0:
+		matriz[2][0]=7
+	elif primer_mov_pos==1:
+		matriz[2][1]=7
+	else:
+		matriz[2][2]=7
+	
+	print(matriz[0])
+	print(matriz[1])
+	print(matriz[2])
+	
+print("Ahora juegan los 8!")
+print("Elija la fila de la 0 a la 2, siendo la primer fila el valor 0")
+segundo_mov_fila=int (input())
+
+if segundo_mov_fila==0:
+	print("Elija la posicion de la 0 a la 2, siendo la primera posicion el valor 0")
+	segundo_mov_pos=int (input())
+	
+	if segundo_mov_pos==0:
+		matriz[0][0]=8
+	elif segundo_mov_pos==1:
+		matriz[0][1]=8
+	else:
+		matriz[0][2]=8
+	
+	print(matriz[0])
+	print(matriz[1])
+	print(matriz[2])
+elif segundo_mov_fila==1:
+	print("Elija la posicion de la 0 a la 2, siendo la primera posicion el valor 0")
+	segundo_mov_pos=int (input())
+	
+	if segundo_mov_pos==0:
+		matriz[1][0]=8
+	elif segundo_mov_pos==1:
+		matriz[1][1]=8
+	else:
+		matriz[1][2]=8
+	
+	print(matriz[0])
+	print(matriz[1])
+	print(matriz[2])
+else:
+	print("Elija la posicion de la 0 a la 2, siendo la primera posicion el valor 0")
+	segundo_mov_pos=int (input())
+	
+	if segundo_mov_pos==0:
+		matriz[2][0]=8
+	elif segundo_mov_pos==1:
+		matriz[2][1]=8
+	else:
+		matriz[2][2]=8
+	
+	print(matriz[0])
+	print(matriz[1])
+	print(matriz[2])
+	
+print("Ahora juegan de nuevo los 7!")
+print("Elija la fila de la 0 a la 2, siendo la primer fila el valor 0")
+tercer_mov_fila=int (input())
+
+if tercer_mov_fila==0:
+	print("Elija la posicion de la 0 a la 2, siendo la primera posicion el valor 0")
+	tercer_mov_pos=int (input())
+	
+	if tercer_mov_pos==0:
+		matriz[0][0]=7
+	elif tercer_mov_pos==1:
+		matriz[0][1]=7
+	else:
+		matriz[0][2]=7
+	
+	print(matriz[0])
+	print(matriz[1])
+	print(matriz[2])
+elif tercer_mov_fila==1:
+	print("Elija la posicion de la 0 a la 2, siendo la primera posicion el valor 0")
+	tercer_mov_pos=int (input())
+	
+	if tercer_mov_pos==0:
+		matriz[1][0]=7
+	elif tercer_mov_pos==1:
+		matriz[1][1]=7
+	else:
+		matriz[1][2]=7
+	
+	print(matriz[0])
+	print(matriz[1])
+	print(matriz[2])
+else:
+	print("Elija la posicion de la 0 a la 2, siendo la primera posicion el valor 0")
+	tercer_mov_pos=int (input())
+	
+	if tercer_mov_pos==0:
+		matriz[2][0]=7
+	elif tercer_mov_pos==1:
+		matriz[2][1]=7
+	else:
+		matriz[2][2]=7
+	
+	print(matriz[0])
+	print(matriz[1])
+	print(matriz[2])
+
+print("Ahora juegan de nuevo los 8!")
+print("Elija la fila de la 0 a la 2, siendo la primer fila el valor 0")
+cuarto_mov_fila=int (input())
+
+if cuarto_mov_fila==0:
+	print("Elija la posicion de la 0 a la 2, siendo la primera posicion el valor 0")
+	cuarto_mov_pos=int (input())
+	
+	if cuarto_mov_pos==0:
+		matriz[0][0]=8
+	elif cuarto_mov_pos==1:
+		matriz[0][1]=8
+	else:
+		matriz[0][2]=8
+	
+	print(matriz[0])
+	print(matriz[1])
+	print(matriz[2])
+elif cuarto_mov_fila==1:
+	print("Elija la posicion de la 0 a la 2, siendo la primera posicion el valor 0")
+	cuarto_mov_pos=int (input())
+	
+	if cuarto_mov_pos==0:
+		matriz[1][0]=8
+	elif cuarto_mov_pos==1:
+		matriz[1][1]=8
+	else:
+		matriz[1][2]=8
+	
+	print(matriz[0])
+	print(matriz[1])
+	print(matriz[2])
+else:
+	print("Elija la posicion de la 0 a la 2, siendo la primera posicion el valor 0")
+	cuarto_mov_pos=int (input())
+	
+	if cuarto_mov_pos==0:
+		matriz[2][0]=8
+	elif cuarto_mov_pos==1:
+		matriz[2][1]=8
+	else:
+		matriz[2][2]=8
+	
+	print(matriz[0])
+	print(matriz[1])
+	print(matriz[2])
+	
+print("Ahora juegan de nuevo los 7!")
+print("Elija la fila de la 0 a la 2, siendo la primer fila el valor 0")
+quinto_mov_fila=int (input())
+
+if quinto_mov_fila==0:
+	print("Elija la posicion de la 0 a la 2, siendo la primera posicion el valor 0")
+	quinto_mov_pos=int (input())
+	
+	if quinto_mov_pos==0:
+		matriz[0][0]=7
+	elif quinto_mov_pos==1:
+		matriz[0][1]=7
+	else:
+		matriz[0][2]=7
+	
+	print(matriz[0])
+	print(matriz[1])
+	print(matriz[2])
+elif quinto_mov_fila==1:
+	print("Elija la posicion de la 0 a la 2, siendo la primera posicion el valor 0")
+	quinto_mov_pos=int (input())
+	
+	if quinto_mov_pos==0:
+		matriz[1][0]=7
+	elif quinto_mov_pos==1:
+		matriz[1][1]=7
+	else:
+		matriz[1][2]=7
+	
+	print(matriz[0])
+	print(matriz[1])
+	print(matriz[2])
+else:
+	print("Elija la posicion de la 0 a la 2, siendo la primera posicion el valor 0")
+	quinto_mov_pos=int (input())
+	
+	if quinto_mov_pos==0:
+		matriz[2][0]=7
+	elif quinto_mov_pos==1:
+		matriz[2][1]=7
+	else:
+		matriz[2][2]=7
+	
+	print(matriz[0])
+	print(matriz[1])
+	print(matriz[2])
+	
+print("Ahora juegan de nuevo los 8!")
+print("Elija la fila de la 0 a la 2, siendo la primer fila el valor 0")
+sexto_mov_fila=int (input())
+
+if sexto_mov_fila==0:
+	print("Elija la posicion de la 0 a la 2, siendo la primera posicion el valor 0")
+	sexto_mov_pos=int (input())
+	
+	if sexto_mov_pos==0:
+		matriz[0][0]=8
+	elif sexto_mov_pos==1:
+		matriz[0][1]=8
+	else:
+		matriz[0][2]=8
+	
+	print(matriz[0])
+	print(matriz[1])
+	print(matriz[2])
+elif sexto_mov_fila==1:
+	print("Elija la posicion de la 0 a la 2, siendo la primera posicion el valor 0")
+	sexto_mov_pos=int (input())
+	
+	if sexto_mov_pos==0:
+		matriz[1][0]=8
+	elif sexto_mov_pos==1:
+		matriz[1][1]=8
+	else:
+		matriz[1][2]=8
+	
+	print(matriz[0])
+	print(matriz[1])
+	print(matriz[2])
+else:
+	print("Elija la posicion de la 0 a la 2, siendo la primera posicion el valor 0")
+	sexto_mov_pos=int (input())
+	
+	if sexto_mov_pos==0:
+		matriz[2][0]=8
+	elif sexto_mov_pos==1:
+		matriz[2][1]=8
+	else:
+		matriz[2][2]=8
+	
+	print(matriz[0])
+	print(matriz[1])
+	print(matriz[2])
+	
+print("Ahora juegan de nuevo los 7!")
+print("Elija la fila de la 0 a la 2, siendo la primer fila el valor 0")
+septimo_mov_fila=int (input())
+
+if septimo_mov_fila==0:
+	print("Elija la posicion de la 0 a la 2, siendo la primera posicion el valor 0")
+	septimo_mov_pos=int (input())
+	
+	if septimo_mov_pos==0:
+		matriz[0][0]=7
+	elif septimo_mov_pos==1:
+		matriz[0][1]=7
+	else:
+		matriz[0][2]=7
+	
+	print(matriz[0])
+	print(matriz[1])
+	print(matriz[2])
+elif septimo_mov_fila==1:
+	print("Elija la posicion de la 0 a la 2, siendo la primera posicion el valor 0")
+	septimo_mov_pos=int (input())
+	
+	if septimo_mov_pos==0:
+		matriz[1][0]=7
+	elif septimo_mov_pos==1:
+		matriz[1][1]=7
+	else:
+		matriz[1][2]=7
+	
+	print(matriz[0])
+	print(matriz[1])
+	print(matriz[2])
+else:
+	print("Elija la posicion de la 0 a la 2, siendo la primera posicion el valor 0")
+	septimo_mov_pos=int (input())
+	
+	if septimo_mov_pos==0:
+		matriz[2][0]=7
+	elif septimo_mov_pos==1:
+		matriz[2][1]=7
+	else:
+		matriz[2][2]=7
+	
+	print(matriz[0])
+	print(matriz[1])
+	print(matriz[2])
+
+print("Ahora juegan de nuevo los 8!")
+print("Elija la fila de la 0 a la 2, siendo la primer fila el valor 0")
+octavo_mov_fila=int (input())
+
+if octavo_mov_fila==0:
+	print("Elija la posicion de la 0 a la 2, siendo la primera posicion el valor 0")
+	octavo_mov_pos=int (input())
+	
+	if octavo_mov_pos==0:
+		matriz[0][0]=8
+	elif octavo_mov_pos==1:
+		matriz[0][1]=8
+	else:
+		matriz[0][2]=8
+	
+	print(matriz[0])
+	print(matriz[1])
+	print(matriz[2])
+elif octavo_mov_fila==1:
+	print("Elija la posicion de la 0 a la 2, siendo la primera posicion el valor 0")
+	octavo_mov_pos=int (input())
+	
+	if octavo_mov_pos==0:
+		matriz[1][0]=8
+	elif octavo_mov_pos==1:
+		matriz[1][1]=8
+	else:
+		matriz[1][2]=8
+	
+	print(matriz[0])
+	print(matriz[1])
+	print(matriz[2])
+else:
+	print("Elija la posicion de la 0 a la 2, siendo la primera posicion el valor 0")
+	octavo_mov_pos=int (input())
+	
+	if octavo_mov_pos==0:
+		matriz[2][0]=8
+	elif octavo_mov_pos==1:
+		matriz[2][1]=8
+	else:
+		matriz[2][2]=8
+	
+	print(matriz[0])
+	print(matriz[1])
+	print(matriz[2])
+	
+print("Ahora juegan de nuevo los 7!")
+print("Elija la fila de la 0 a la 2, siendo la primer fila el valor 0")
+noveno_mov_fila=int (input())
+
+if noveno_mov_fila==0:
+	print("Elija la posicion de la 0 a la 2, siendo la primera posicion el valor 0")
+	noveno_mov_pos=int (input())
+	
+	if noveno_mov_pos==0:
+		matriz[0][0]=7
+	elif noveno_mov_pos==1:
+		matriz[0][1]=7
+	else:
+		matriz[0][2]=7
+	
+	print(matriz[0])
+	print(matriz[1])
+	print(matriz[2])
+elif noveno_mov_fila==1:
+	print("Elija la posicion de la 0 a la 2, siendo la primera posicion el valor 0")
+	noveno_mov_pos=int (input())
+	
+	if noveno_mov_pos==0:
+		matriz[1][0]=7
+	elif noveno_mov_pos==1:
+		matriz[1][1]=7
+	else:
+		matriz[1][2]=7
+	
+	print(matriz[0])
+	print(matriz[1])
+	print(matriz[2])
+else:
+	print("Elija la posicion de la 0 a la 2, siendo la primera posicion el valor 0")
+	noveno_mov_pos=int (input())
+	
+	if noveno_mov_pos==0:
+		matriz[2][0]=8
+	elif noveno_mov_pos==1:
+		matriz[2][1]=8
+	else:
+		matriz[2][2]=8
+	
+	print(matriz[0])
+	print(matriz[1])
+	print(matriz[2])
